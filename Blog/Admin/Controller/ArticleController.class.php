422ac51699ba95168611489d74752143142e4779
unknown <>
2017-01-01 21:01:42 +0800

    2017-01-01

--- Blog/Admin/Controller/ArticleController.class.php
+++ Blog/Admin/Controller/ArticleController.class.php
@@ -3,12 +3,15 @@ namespace Admin\Controller;
 use Common\Controller\BaseController;
 
 class ArticleController extends BaseController {
+    /**文章列表*/
     public function index(){
+
         $this->display();
     }
 
     /**添加文章*/
     public function addtext(){
+
         $this->display();
     }
 
@@ -19,6 +22,15 @@ class ArticleController extends BaseController {
 
     /**删除文章*/
     public function deltext(){
+        $this->display();
+    }
+
+    /*文章作者*/
+    public function lists(){
+        $this->display();
+    }
+
+    public function addauthor(){
         $this->display();
     }
     
