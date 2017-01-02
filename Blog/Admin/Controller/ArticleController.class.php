5f8e6f95b85d3359efe1b5e6501fced2c179c52b
unknown <>
2017-01-02 16:29:52 +0800

    2017-01-02

--- Blog/Admin/Controller/ArticleController.class.php
+++ Blog/Admin/Controller/ArticleController.class.php
@@ -5,13 +5,32 @@ use Common\Controller\BaseController;
 class ArticleController extends BaseController {
     /**文章列表*/
     public function index(){
-
+        $info =M('article')->field('*')->where('is_delete=1')->order('id desc')->select();
+        foreach($info as $k => $v){
+            $info[$k]['cate_name']=M('category')->where('id='.$v['cate_id'])->getField('cate_name');
+        }
+        foreach($info as $K => $v){
+            $info[$k]['author']=M('author')->where('id='.$v['author_id'])->where('status=1')->getField('author');
+        }
+        $this->assign('info',$info);
         $this->display();
     }
 
     /**添加文章*/
     public function addtext(){
-
+        if(IS_POST){
+            $_POST['title'] =I('post.title');
+            $_POST['add_time'] =time();
+            $_POST['is_show'] =I('post.is_show');
+            $_POST['author'] =I('post.author');
+            $_POST['content'] =I('post.content');
+            $_POST['cate_id'] =I('post.cate_id');
+            M('article')->add($_POST);
+        }
+        $author = M('author')->field('*')->where('status=1')->select();
+        $this->assign('author',$author);
+        $category = M('category')->field('*')->select();
+        $this->assign('category',$category);
         $this->display();
     }
 
@@ -27,7 +46,7 @@ class ArticleController extends BaseController {
 
     /*文章作者*/
     public function lists(){
-       $list= M('author')->field('author,status')->where('status=1')->order('id desc,status desc')->select();
+        $list= M('author')->field('author,status')->where('status=1')->order('id desc,status desc')->select();
         $this->assign('list',$list);
         $this->display();
     }
@@ -41,5 +60,19 @@ class ArticleController extends BaseController {
         }
         $this->display();
     }
-    
+
+    public function category(){
+        $category= M('category')->field('id,cate_name')->where('')->order('id desc')->select();
+        $this->assign('category',$category);
+        $this->display();
+    }
+
+    public function addcate(){
+        if(IS_POST){
+            $data['cate_name'] =$_POST['cate_name'];
+            M('category')->data($data)->add();
+            $this->redirect('category');
+        }
+        $this->display();
+    }
 }
