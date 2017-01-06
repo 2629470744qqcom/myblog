62171f8dce05ff1e3d21fac2239a364580a9a36c
wangrun <2629470744@qq.com>
2017-01-06 14:16:14 +0800

    2017-01-06

---
+++ Blog/Admin/Controller/TagController.class.php
@@ -0,0 +1,39 @@
+<?php
+ namespace Admin\Controller;
+ use Common\Controller\BaseController;
+
+ class TagController extends BaseController{
+     public function tag(){
+         $tag= M('tag')->field('*')->order('id desc,status desc')->select();
+         $this->assign('tag',$tag);
+         $this->display();
+     }
+
+     public function add(){
+         if(IS_POST){
+             M('tag')->add($_POST);
+             $this->redirect('tag');
+         }
+         $this->assign('action_url', U('Tag/add'));
+         $this->display();
+     }
+
+     public function edit(){
+         if(IS_POST){
+             $_POST['status']=I('post.status');
+             $_POST['tag'] =I('post.tag');
+             M('tag')->where('id='.I('post.id',0,'intval'))->save($_POST);
+             $this->redirect('tag');
+         }
+         $infos= M('tag')->field('*')->where('id='.I('get.id',0,'intval'))->find();
+        // dump($infos);die;
+         $this->assign('infos',$infos);
+         $this->assign('action_url', U('Tag/edit'));
+         $this->display('add');
+     }
+
+     public function del(){
+         M('tag')->where('id='.I('get.id',0,'intval'))->delete();
+         $this->redirect('tag');
+     }
+ }
--- Blog/Home/Controller/IndexController.class.php
+++ Blog/Home/Controller/IndexController.class.php
@@ -2,24 +2,51 @@
 namespace Home\Controller;
 use Common\Controller\BaseController;
 
+/*博客首页*/
 class IndexController extends BaseController {
+     //首页
     public function index(){
         $where = 'a.is_show=1 and a.is_delete=1 and u.status=1';
         $where .= ' and a.author_id=u.id';
         $where .= ' and a.cate_id=c.id';
-        $list =M()->table('rain_article as a,rain_category as c,rain_author as u')->field('a.pic,a.hit,a.add_time,a.title,a.content,u.name as uname,c.cate_name as cname')->where($where)->order('a.add_time desc')->select();
-
-        //dump($list);die;
+        $list =M()->table('rain_article as a,rain_category as c,rain_author as u')->field('a.cate_id,a.id,a.pic,a.hit,a.add_time,a.title,a.content,u.name as uname,c.cate_name as cname')->where($where)->order('a.add_time desc')->select();
         $this->assign('list',$list);
-
         $category =M('category')->field('*')->select();
         $this->assign('category',$category);
-        //dump($category);die;
         $this->display();
     }
 
+    //文章详情页
     public function article(){
+        $info = M('article')->field('*')->where('id='.I('get.id',0,'intval'))->find();
+        $info['author'] = M('author')->where('status=1 and id='.$info['author_id'])->getField('name');
+        $info['category'] = M('category')->where('id='.$info['cate_id'])->getField('cate_name');
+
+        $new_hit =$info['hit'] +1 ;
+        M('article')->where('id='.$info['id'])->setField('hit',$new_hit);
+        $info['hit'] +=1;
+        $this->assign('info',$info);
+
+        $category =M('category')->field('*')->select();
+        $this->assign('category',$category);
+        $this->display();
+    }
+
+    //分类
+    public function category(){
+        $categorys = M('article')->field('*')->where('cate_id='.I('get.cate_id'))->select();
+        foreach($categorys as $k =>$v){
+            $categorys[$k]['cate']=M('category')->where('id='.$v['cate_id'])->getField('cate_name');
+        }
+        foreach($categorys as $k =>$v){
+            $categorys[$k]['author']=M('author')->where('id='.$v['author_id'])->getField('name');
+        }
+        $this->assign('categorys',$categorys);
+
+        $category =M('category')->field('*')->select();
+        $this->assign('category',$category);
 
         $this->display();
+
     }
 }
